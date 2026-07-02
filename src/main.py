43e6b6b05c9eca8c3324132e19bc66a7d1ec1727
CamylleMillene06 <Camylle.millene@ufpe.br>
2026-07-02 13:43:25 -0300

    Exibir informações do grafo

--- src/main.py
+++ src/main.py
@@ -5,5 +5,4 @@ grafo = Grafo()
 
 grafo.carregar_csv("../data/D2_prereqs-nicen.csv")
 
-print("Quantidade de vértices:", len(grafo.vertices))
-print("Quantidade de arestas:", grafo.arestas)
+grafo.mostrar_informacoes()
