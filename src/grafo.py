cea99c3eae1edb141b1768b7dccdc13b073c7ad7
CamylleMillene06 <Camylle.millene@ufpe.br>
2026-07-02 13:21:20 -0300

    Implementando a estrutura básica da classe Grafo

--- src/grafo.py
+++ src/grafo.py
@@ -1,1 +1,20 @@
-# Classe principal do grafo
+import csv
+
+
+class Grafo:
+
+    def __init__(self):
+        self.adj = {}
+        self.vertices = set()
+        self.arestas = 0
+
+    def adicionar_aresta(self, origem, destino):
+
+        self.vertices.add(origem)
+        self.vertices.add(destino)
+
+        if origem not in self.adj:
+            self.adj[origem] = []
+
+        self.adj[origem].append(destino)
+        self.arestas += 1
