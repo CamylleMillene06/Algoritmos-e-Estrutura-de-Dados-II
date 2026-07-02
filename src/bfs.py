51e53b42794e3486b50ae0031d44f02267c01c85
CamylleMillene06 <Camylle.millene@ufpe.br>
2026-07-02 18:52:43 -0300

    Percurso da BFS

--- src/bfs.py
+++ src/bfs.py
@@ -4,3 +4,28 @@ class BFS:
 
     def __init__(self, grafo):
         self.grafo = grafo
+
+    def buscar(self, inicio):
+
+        visitados = set()
+        fila = deque()
+
+        fila.append(inicio)
+        visitados.add(inicio)
+
+        while fila:
+
+            atual = fila.popleft()
+
+            print(atual)
+            
+            if atual in self.grafo.adj:
+
+                for vizinho in self.grafo.adj[atual]:
+
+                    if vizinho not in visitados:
+
+                        visitados.add(vizinho)
+                        fila.append(vizinho)
+
+                
