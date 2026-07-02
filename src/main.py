eaf490b68222dd3b44e9cc24adcb6ca093195ea7
CamylleMillene06 <Camylle.millene@ufpe.br>
2026-07-02 13:33:03 -0300

    Carregamento inicial do grafo

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-# Programa principal
+from grafo import Grafo
+
+
+grafo = Grafo()
+
+grafo.carregar_csv("../data/D2_prereqs-nicen.csv")
+
+print("Quantidade de vértices:", len(grafo.vertices))
+print("Quantidade de arestas:", grafo.arestas)
