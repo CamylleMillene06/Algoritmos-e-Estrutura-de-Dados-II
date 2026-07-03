69fe88b07b4f660cb2c607f7492c3a27995495c8
CamylleMillene06 <Camylle.millene@ufpe.br>
2026-07-03 13:47:14 -0300

    Método principal da DFS

--- src/dfs.py
+++ src/dfs.py
@@ -2,3 +2,12 @@ class DFS:
 
     def __init__(self, grafo):
         self.grafo = grafo
+
+    def buscar(self, inicio):
+
+        visitados = set()
+        ordem = []
+
+        self._dfs(inicio, visitados, ordem)
+
+        return ordem
