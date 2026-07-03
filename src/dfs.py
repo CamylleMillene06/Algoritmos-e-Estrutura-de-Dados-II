b5ddc462e895b7d72fead29771095162aa80cbb1
CamylleMillene06 <Camylle.millene@ufpe.br>
2026-07-03 13:51:52 -0300

    Ordem de visita na DFS

--- src/dfs.py
+++ src/dfs.py
@@ -11,3 +11,16 @@ class DFS:
         self._dfs(inicio, visitados, ordem)
 
         return ordem
+
+    def _dfs(self, vertice, visitados, ordem):
+
+        visitados.add(vertice)
+        ordem.append(vertice)
+
+        if vertice in self.grafo.adj:
+
+            for vizinho in self.grafo.adj[vertice]:
+
+                if vizinho not in visitados:
+
+                    self._dfs(vizinho, visitados, ordem)
